334bd378ba016bb2766d5a2bcfa43874351d0511
flaviovich <flaviovichdev@gmail.com>
2022-10-14 18:43:04 -0500

    fixed listar & crear router

--- app/routers/marca_router.py
+++ app/routers/marca_router.py
@@ -3,13 +3,15 @@ from flask import request
 from app.controllers.marca_controller import MarcaController
 
 @app.route("/marcas", methods=['GET'])
-def listar():
-    return MarcaController().getAll()
+def listar():  # sourcery skip: inline-immediately-returned-variable
+    marcas = MarcaController().getAll()
+    return marcas
 
 @app.route("/marca", methods=['POST'])
-def crear():
+def crear():  # sourcery skip: inline-immediately-returned-variable
     json_input = request.get_json()
-    return MarcaController().post(json_input)
+    result = MarcaController().post(json_input)
+    return result
 
 @app.route("/marca/<int:id>", methods=['PUT'])
 def actualizar(id):
