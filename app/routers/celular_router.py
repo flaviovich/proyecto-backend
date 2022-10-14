3f174660faec22c0692f3e42732f86ca4b6ef30a
flaviovich <flaviovichdev@gmail.com>
2022-10-14 18:59:06 -0500

    fixed celular & marca routers

--- app/routers/celular_router.py
+++ app/routers/celular_router.py
@@ -8,19 +8,19 @@ def listarCelulares():  # sourcery skip: inline-immediately-returned-variable
     return celulares
 
 @app.route("/celular", methods=['POST'])
-def crear():  # sourcery skip: inline-immediately-returned-variable
+def crearCelular():  # sourcery skip: inline-immediately-returned-variable
     json_input = request.get_json()
     result = CelularController().post(json_input)
     return result
 
 @app.route("/celular/<int:id>", methods=['PUT'])
-def actualizar(id):
+def actualizarCelular(id):
     celular = CelularController()
     json_input = request.get_json()
     return celular.update(id,json_input)
 
 @app.route("/celular/<int:id>", methods=['DELETE'])
-def eliminar(id):
+def eliminarCelular(id):
     celular = CelularController()
     return celular.delete(id)
 
--- app/routers/marca_router.py
+++ app/routers/marca_router.py
@@ -8,18 +8,18 @@ def listarMarcas():  # sourcery skip: inline-immediately-returned-variable
     return marcas
 
 @app.route("/marca", methods=['POST'])
-def crear():  # sourcery skip: inline-immediately-returned-variable
+def crearMarca():  # sourcery skip: inline-immediately-returned-variable
     json_input = request.get_json()
     result = MarcaController().post(json_input)
     return result
 
 @app.route("/marca/<int:id>", methods=['PUT'])
-def actualizar(id):
+def actualizarMarca(id):
     marca = MarcaController()
     json_input = request.get_json()
     return marca.update(id, json_input)
 
 @app.route("/celular/<int:id>", methods=['DELETE'])
-def eliminar(id):
+def eliminarMarca(id):
     marca = MarcaController()
     return marca.delete(id)
