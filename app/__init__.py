e6fb91261534e1aff04bed0cbac650e16403164a
flaviovich <flaviovichdev@gmail.com>
2022-10-19 14:16:12 -0500

    added migrations

--- app/__init__.py
+++ app/__init__.py
@@ -4,20 +4,20 @@ from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
-from logging import (
-    basicConfig, 
-    DEBUG, 
-    FileHandler, 
-    StreamHandler
-)
+# from logging import (
+#     basicConfig, 
+#     DEBUG, 
+#     FileHandler, 
+#     StreamHandler
+# )
 import os
 from datetime import timedelta
 
-basicConfig(
-    level=DEBUG,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[FileHandler('flask.log'), StreamHandler()]
-)
+# basicConfig(
+#     level=DEBUG,
+#     format="%(asctime)s [%(levelname)s] %(message)s",
+#     handlers=[FileHandler('flask.log'), StreamHandler()]
+# )
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
